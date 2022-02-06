1e86fcc6a7f5689d54b2edf1bc5fc2f0fe24329d
MVbys <bismaxmail@gmail.com>
2022-02-06 17:05:14 +0200

    complitedContest view corected

--- resources/views/gallery/complitedContest.blade.php
+++ resources/views/gallery/complitedContest.blade.php
@@ -12,18 +12,18 @@
 
 
                 <div class="card card_center card_winner animated bounceIn">
-                    <div class="card__active-wrapper">
-                        <p class="card__active-text card-title card-title_yellow">
+                    {{-- <div class="card__active-wrapper"> --}}
+                        {{-- <p class="card__active-text card-title card-title_yellow">
                             Активен
-                        </p>
-                        <div class="card__active-icon">
+                        </p> --}}
+                        {{-- <div class="card__active-icon">
                             <svg width="35" height="32" viewBox="0 0 35 32" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path
                                     d="M17.5 0L21.429 12.0922L34.1435 12.0922L23.8572 19.5656L27.7862 31.6578L17.5 24.1844L7.21376 31.6578L11.1428 19.5656L0.85651 12.0922L13.571 12.0922L17.5 0Z"
                                     fill="#EBFF00" />
                             </svg>
-                        </div>
-                    </div>
+                        </div> --}}
+                    {{-- </div> --}}
 
                     @if ($loop->first)
                         <div class="card__cup image">
